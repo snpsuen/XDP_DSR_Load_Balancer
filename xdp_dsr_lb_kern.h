b90fe1d77cec7f22d58606a48513fab34cd0310c
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2023-06-15 16:15:31 +0800

    Update xdp_dsr_lb_kern.h

--- xdp_dsr_lb_kern.h
+++ xdp_dsr_lb_kern.h
@@ -11,23 +11,10 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
-static __always_inline __u16
-csum_fold_helper(__u64 csum)
-{
-    int i;
-#pragma unroll
-    for (i = 0; i < 4; i++)
-    {
-        if (csum >> 16)
-            csum = (csum & 0xffff) + (csum >> 16);
-    }
-    return ~csum;
-}
+#define IP_ADDRESS(x) (unsigned int)(172 + (17 << 8) + (0 << 16) + (x << 24))
+#define VIP_ADDRESS(x) (unsigned int)(192 + (168 << 8) + (25 << 16) + (x << 24))
 
-static __always_inline __u16
-iph_csum(struct iphdr *iph)
-{
-    iph->check = 0;
-    unsigned long long csum = bpf_csum_diff(0, 0, (unsigned int *)iph, sizeof(struct iphdr), 0);
-    return csum_fold_helper(csum);
-}
+#define LB 2
+#define BACKEND_A 3
+#define BACKEND_B 4
+#define VIP 10
